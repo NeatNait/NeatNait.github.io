34ce69cf6c1ddc505adfa8cc4962db3cd39786b3
Alvaro Pinot <acpinot@ID09379.urbaser.local>
2014-02-24 14:44:29 +0100

    agreada persistencia de formularios simple

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
- var express = require("express");
- var app = express();
+var storage = require('node-persist'),
+	express = require("express"),
+	app = express();
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendfile('index.html')
- });
 
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     //console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]); 
- });
+//you must first call storage.init or storage.initSync
+storage.initSync();
 
- var port = process.env.PORT || 8989;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+app.use(express.bodyParser()); // Automatically parses form data
+
+
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('index.html')
+});
+
+
+app.post('/contact', function(req, res){ // Specifies which URL to listen for
+
+	var contactInfo = {
+	    headers: req.headers,
+	    body: req.body,
+	    remoteAddress: req.connection.remoteAddress
+	};
+
+	storage.setItem(Date.now()+'_contactInfo.json', contactInfo);
+
+	res.redirect('/')
+});
+
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){ 
+	//console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]); 
+});
+
+
+
+var port = process.env.PORT || 3000;
+	app.listen(port, function() {
+	console.log("Listening on " + port);
+});
